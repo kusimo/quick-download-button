8411684e8fc128d5197e337830f7dd8d9b7c304d
Abidemi Kusimo <abidemi@MacBook-Pro.local>
2020-09-05 19:47:50 +0100

    Shortcode added

--- custom-download.php
+++ custom-download.php
@@ -79,3 +79,103 @@ function custom_download_register_blocks() {
 
 }
 
+//Shortcode option
+
+
+function custom_download_button_shortcode($atts, $content = null) {
+    $a = shortcode_atts(array(
+        'title' => 'Download',
+        'bgcolor' => 'default',
+        'filesize' => '',
+        'duration' => '',
+        'url' => '',
+        'hotlink' => false,
+    ), $atts);
+
+    global $post;
+    $pid = $post->ID;
+    $hotlink_url = '/dl.php?aid='.$pid;
+    
+
+    ob_start();
+    ?>
+
+    <div class="button--download" id="bsing-dlbtn" style="margin: 6rem auto;width: 200px;">
+      <form method="post" action="<?php echo !empty($a['url'])? $a['url'] : $hotlink_url ; ?>">
+      <button class="g-btn f-l bsbtn d-block position-relative shadow rounded-lg border-0" type="submit" style="z-index:2;height:50px; width:200px;" title="Download" data-pid="<?php echo $pid; ?>"><?php echo $a['title']; ?></button>
+      </form>
+        <?php if('' != $a['duration']) : ?>
+        <p class="up"><i class="fa fa-clock-o"></i> <?php echo $a['duration']; ?></p>
+        <?php endif ; ?>
+
+        <?php if( '1' === $a['filesize'] ) : ?>
+        
+        <?php 
+        echo '<p class="down"><i class="fa fa-folder-o"></i> ';
+            $file_url = filesize(convert_url_to_path($a['url']) );
+            $file_size = formatSizeUnits($file_url);
+            if('0 bytes' != $file_size) echo formatSizeUnits($file_url);
+        echo '</p>';
+
+        elseif('' !== $a['filesize']) :
+        echo '<p class="down"><i class="fa fa-folder-o"></i> ';
+                echo $a['filesize'];
+        echo '</p>';
+
+        //else :
+        ?>
+      
+        <?php endif; ?>
+
+    </div>
+
+<?php
+    return ob_get_clean();
+}
+
+add_shortcode('custom_download', 'custom_download_button_shortcode');
+
+//convert URL to path
+if(!function_exists('convert_url_to_path')) {
+    function convert_url_to_path( $url ) {
+        return str_replace( 
+            wp_get_upload_dir()['baseurl'], 
+            wp_get_upload_dir()['basedir'], 
+            $url
+        );
+      }
+}
+
+
+//get file size
+if(!function_exists('formatSizeUnits')) {
+	function formatSizeUnits($bytes)
+    {
+        if ($bytes >= 1073741824)
+        {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        }
+        elseif ($bytes >= 1048576)
+        {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        }
+        elseif ($bytes >= 1024)
+        {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        }
+        elseif ($bytes > 1)
+        {
+            $bytes = $bytes . ' bytes';
+        }
+        elseif ($bytes == 1)
+        {
+            $bytes = $bytes . ' byte';
+        }
+        else
+        {
+            $bytes = '0 bytes';
+        }
+
+        return $bytes;
+}
+}
